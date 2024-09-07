6b16c4e674830116fd8e719f9c1e423a88b686e0
jeremyn-nmq <quannguyen.jn@gmail.com>
2024-09-08 02:43:50 +0700

    part 1

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,45 +4,47 @@ import service2 from "../../images/service2.png";
 import service3 from "../../images/service3.png";
 import {useTranslation} from "gatsby-plugin-react-i18next";
 
-
 const Services = () => {
     const {t} = useTranslation();
     return (
-        <div className={"service-section bg-[#0068B0] flex flex-col gap-12"} id={"services"}>
-            <div className={"flex align-top gap-[60px] md:flex-row flex-col"}>
-                <h2 className={"heading-section"}>
-                    {t("services_heading")}
-                </h2>
-                <p className={"text-white"}>
-                    {t("services_description")}
-                </p>
-            </div>
-            <div className={"flex justify-center md:justify-between md:gap-0 gap-5 md:flex-row flex-col"}>
-                <div className={"service-item"}>
-                    <img className={"service-image w-full"} src={service1} alt={""}/>
-                    <p className={"service-name"}>
-                        {t("services_first_service_item")}
-                        <span className={"service-index"}>1</span>
+        <div className={"container-main container-main--right"} id={"services"}>
+            <div className={"one-side one-side--from-left service-section bg-[#0068B0] flex flex-col gap-12"}>
+                <div className={"service-section--wrapper flex align-top gap-[3rem] justify-start"}>
+                    <h2 className={"heading-section"}>
+                        {t("services_heading")}
+                    </h2>
+                    <p className={"heading-section--description text-white"}>
+                        {t("services_description")}
                     </p>
                 </div>
-                <div className={"service-item"}>
-                    <p className={"service-name md:flex hidden"}>
-                        {t("services_second_service_item")}
-                        <span className={"service-index bottom-index"}>2</span>
-                    </p>
-                    <img className={"service-image w-full"} src={service2} alt={""}/>
-                    <p className={"service-name md:hidden flex justify-center"}>
-                        {t("services_second_service_item")}
-                        <span className={"service-index"}>2</span>
-                    </p>
+                <div className={"service-section--wrapper flex justify-center md:justify-between"}>
+                    <div className={"service-item"}>
+                        <img className={"service-image w-full"} src={service1} alt={""}/>
+                        <p className={"service-name"}>
+                            {t("services_first_service_item")}
+                            <span className={"service-index"}>1</span>
+                        </p>
+                    </div>
+                    <div className={"service-item"}>
+                        <p className={"service-name show-on-desktop"}>
+                            {t("services_second_service_item")}
+                            <span className={"service-index bottom-index"}>2</span>
+                        </p>
+                        <img className={"service-image w-full"} src={service2} alt={""}/>
+                        <p className={"service-name show-on-mobile flex justify-center"}>
+                            {t("services_second_service_item")}
+                            <span className={"service-index"}>2</span>
+                        </p>
+                    </div>
+                    <div className={"service-item"}>
+                        <img className={"service-image w-full"} src={service3} alt={""}/>
+                        <p className={"service-name"}>
+                            {t("services_third_service_item")}
+                            <span className={"service-index"}>3</span>
+                        </p>
+                    </div>
                 </div>
-                <div className={"service-item"}>
-                    <img className={"service-image w-full"} src={service3} alt={""}/>
-                    <p className={"service-name"}>
-                        {t("services_third_service_item")}
-                        <span className={"service-index"}>3</span>
-                    </p>
-                </div>
+
             </div>
         </div>
     )
--- src/components/TransportRoutes/TransportRoutes.jsx
+++ src/components/TransportRoutes/TransportRoutes.jsx
@@ -1,36 +1,24 @@
 import * as React from "react"
-import flag1 from "../../images/flag1.png";
-import flag2 from "../../images/flag2.png";
-import flag3 from "../../images/flag3.png";
-import flag4 from "../../images/flag4.png";
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 
-
-const TransportRoutes = () => {
+const TransportRoutes = ({transportRoutes}) => {
     const {t} = useTranslation();
+
     return (
-        <div className={"container mx-auto flex flex-col gap-5"} id={"routes"}>
+        <div className={"container-main container-main--even flex flex-col items-center justify-center gap-5"} id={"routes"}>
             <h2 className={"heading-section text-center"}>ROUTES</h2>
             <p className={"text-center opacity-60"}>
                 {t("routes_description")}
             </p>
-            <div className={"flex columns-4 md:justify-between justify-center flex-wrap md:gap-0 gap-5"}>
-                <div className={"flex flex-col gap-5"}>
-                    <img src={flag1} alt={""}/>
-                    <p className={"text-center route-name"}>Viet Nam</p>
-                </div>
-                <div className={"flex flex-col gap-5"}>
-                    <img src={flag2} alt={""}/>
-                    <p className={"text-center route-name"}>China</p>
-                </div>
-                <div className={"flex flex-col gap-5"}>
-                    <img src={flag3} alt={""}/>
-                    <p className={"text-center route-name"}>Cambodia</p>
-                </div>
-                <div className={"flex flex-col gap-5"}>
-                    <img src={flag4} alt={""}/>
-                    <p className={"text-center route-name"}>Laos</p>
-                </div>
+            <div className={"transport-routes-flags"}>
+                {
+                    transportRoutes?.map(tr => (
+                        <div className={"flex flex-col items-center sm:gap-5 gap-3"} key={tr.id}>
+                            <img src={tr.flag} alt={tr.alt}/>
+                            <p className={"text-center route-name"}>{t(`routes_country_${tr.id}`)}</p>
+                        </div>
+                    ))
+                }
             </div>
 
         </div>
