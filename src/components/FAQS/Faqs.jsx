c089dd2fefcd32add3e07d1d813d7ba668e7bc21
jeremyn-nmq <quannguyen.jn@gmail.com>
2024-09-08 19:23:22 +0700

    part 2

--- src/components/FAQS/Faqs.jsx
+++ src/components/FAQS/Faqs.jsx
@@ -39,25 +39,28 @@ const Faqs = () => {
         }
     }
     return (
-        <div className={'flex flex-col gap-5 faq-section flex-wrap'} id={"faqs"}>
-            <h2 className={"heading-section"}>FAQs</h2>
-            <div className={"faq-accordion flex flex-col gap-3 flex-wrap"}>
-                {FAQ.map((item, index) => {
-                    return (
-                        <Flowbite theme={{ theme: customTheme }} key={index}>
-                            <Accordion collapseAll>
-                                <Accordion.Panel>
-                                    <Accordion.Title>{t(item.question)}</Accordion.Title>
-                                    <Accordion.Content>
-                                        <p className="mb-2 text-gray-500 dark:text-gray-400">
-                                            {t(item.answer)}
-                                        </p>
-                                    </Accordion.Content>
-                                </Accordion.Panel>
-                            </Accordion>
-                        </Flowbite>
+        <div className={'container-main container-main--right'}>
+            <div className={'one-side one-side--from-left flex flex-col gap-5 faq-section flex-wrap'} id={"faqs"}>
+                <h2 className={"heading-section"}>FAQs</h2>
+                <div className={"faq-accordion flex flex-col gap-3 flex-wrap"}>
+                    {FAQ.map((item, index) => {
+                            return (
+                                <Flowbite theme={{theme: customTheme}} key={index}>
+                                    <Accordion collapseAll>
+                                        <Accordion.Panel>
+                                            <Accordion.Title>{t(item.question)}</Accordion.Title>
+                                            <Accordion.Content>
+                                                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                                                    {t(item.answer)}
+                                                </p>
+                                            </Accordion.Content>
+                                        </Accordion.Panel>
+                                    </Accordion>
+                                </Flowbite>
+                            )
+                        }
                     )}
-                )}
+                </div>
             </div>
         </div>
     )
